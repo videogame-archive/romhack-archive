9a7b2b81e0a8536b9a7ffb9f3a24032a0b9ec700
Videogame Archive <vg-archive@protonmail.com>
2023-06-04 17:31:44 +0200

    better defaults

--- source/src/test/java/com/github/videogamearchive/database/romset/RomsetCreatorTest.java
+++ source/src/test/java/com/github/videogamearchive/database/romset/RomsetCreatorTest.java
@@ -1,11 +1,10 @@
 package com.github.videogamearchive.database.romset;
 
-import com.github.videogamearchive.database.romset.RomsetCreator;
 import org.junit.jupiter.api.Test;
 
 public class RomsetCreatorTest {
     @Test
     public void romsetCreatorTest() throws Exception {
-        RomsetCreator.main(new String[]{ "archiveRoot", "inputRomRoot", "outputRootRoot"});
+        RomsetCreator.main(new String[]{ "../database", "../roms", "../out", "--validate"});
     }
 }
